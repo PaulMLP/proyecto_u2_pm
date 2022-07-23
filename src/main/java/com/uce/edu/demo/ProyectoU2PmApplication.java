1ed92d12129d24299195f4ec2450dd9ca0313544
Paul Merizalde <merizaldepaul1999@gmail.com>
2022-07-22 20:01:01 -0500

    one to many

--- src/main/java/com/uce/edu/demo/ProyectoU2PmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2PmApplication.java
@@ -1,16 +1,15 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.Ciudadano;
-import com.uce.edu.demo.repository.modelo.Pasaporte;
-import com.uce.edu.demo.service.ICiudadanoService;
+import com.uce.edu.demo.repository.modelo.onetomany.Habitacion;
+import com.uce.edu.demo.repository.modelo.onetomany.Hotel;
+import com.uce.edu.demo.service.IHabitacionService;
+import com.uce.edu.demo.service.IHotelService;
 
 @SpringBootApplication
 public class ProyectoU2PmApplication implements CommandLineRunner {
@@ -18,7 +17,10 @@ public class ProyectoU2PmApplication implements CommandLineRunner {
 	private static Logger LOG = Logger.getLogger(ProyectoU2PmApplication.class);
 
 	@Autowired
-	private ICiudadanoService ciudadanoService;
+	private IHotelService hotelService;
+	
+	@Autowired
+	private IHabitacionService habitacionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2PmApplication.class, args);
@@ -27,33 +29,39 @@ public class ProyectoU2PmApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Ciudadano ciudadano = new Ciudadano();
-		ciudadano.setNombre("David");
-		ciudadano.setApellido("Mendez");
-		ciudadano.setCedula("16486816");
-		ciudadano.setFechaNacimiento(LocalDateTime.now());
-
-		Pasaporte pasaporte = new Pasaporte();
-		pasaporte.setNumero(555);
-		pasaporte.setFechaEmision(LocalDateTime.now());
-		pasaporte.setFechaCaducidad(LocalDateTime.now());
-		pasaporte.setCiudadano(ciudadano);
-
-		ciudadano.setPasaporte(pasaporte);
-
-		// Insertar ciudadano
-		LOG.info("Insercion de : "+ ciudadano);
-		this.ciudadanoService.insertar(ciudadano);
+		Hotel hotel1 = new Hotel();
+		hotel1.setNombre("Hilton Colon UIO");
+		hotel1.setDireccion("La patria");
 		
+		Hotel hotel2 = new Hotel();
+		hotel2.setNombre("Hilton Colon GYU");
+		hotel2.setDireccion("El Malecon");
 		
-		// Buscar ciudadano
-		Ciudadano c = this.ciudadanoService.buscar("16486816");
-		LOG.info("Busqueda: " + c);
+		LOG.info("Se inserta el Hotel: "+hotel1);
+		this.hotelService.insertar(hotel1);
 		
-		// Actualizar ciudadano
-		LOG.info("Se Actualizo: " + this.ciudadanoService.actualizar("16486816", "Hugo"));
-
-		// Eliminar ciudadano
-		LOG.info("Se Elimino" + this.ciudadanoService.eliminar("16486816"));
+		LOG.info("Se inserta el Hotel: "+hotel2);
+		this.hotelService.insertar(hotel2);
+		
+//		Hotel hotel = new Hotel();
+//		hotel.setId(1);
+//		
+//		Habitacion habi1 = new Habitacion();
+//		habi1.setNumero("B10");
+//		habi1.setPiso("3");
+//		habi1.setTipo("Familiar");
+//		habi1.setHotel(hotel);
+//		
+//		Habitacion habi2 = new Habitacion();
+//		habi2.setNumero("C20");
+//		habi2.setPiso("4");
+//		habi2.setTipo("Matrimonial");
+//		habi2.setHotel(hotel);
+//		
+//		LOG.info("Se inserta la habitacion: "+habi1);
+//		this.habitacionService.insertar(habi1);
+//		
+//		LOG.info("Se inserta la habitacion: "+habi2);
+//		this.habitacionService.insertar(habi2);
 	}
 }
--- src/main/java/com/uce/edu/demo/repository/CiudadanoRepositoryImpl.java
+++ src/main/java/com/uce/edu/demo/repository/CiudadanoRepositoryImpl.java
@@ -8,7 +8,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
-import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.onetoone.Ciudadano;
 
 @Repository
 @Transactional
